2322a19e22f9e2a0c7f6a56f35717145265b3630
simran <56672056+simran907@users.noreply.github.com>
2023-08-03 13:24:37 +0530

    Add files via upload

---
+++ Simranpatel_LAB6-Solution/studentfest/src/main/java/com/greatlearning/studentfest/entity/Role.java
@@ -0,0 +1,29 @@
+package com.greatlearning.studentfest.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "role_id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
